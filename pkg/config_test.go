6feebdd4383e1f1cfa12cb4a7c1b498d7f8e463f
brumhard <fobytobi@googlemail.com>
2020-11-16 20:34:42 +0100

    added test cases for nested configs

--- pkg/config_test.go
+++ pkg/config_test.go
@@ -91,7 +91,7 @@ func TestCollector_Env_UnmarshalText(t *testing.T) {
 	r.Equal([]string{"l", "t", "l"}, testStruct.SomeThing.S)
 }
 
-func TestCollector_FileOverwrite(t *testing.T) {
+func TestCollector_File_Overwrite(t *testing.T) {
 	r := require.New(t)
 
 	testStruct := struct {
@@ -117,6 +117,35 @@ func TestCollector_FileOverwrite(t *testing.T) {
 	r.Equal(4000, testStruct.Port)
 }
 
+func TestCollector_File_Nested(t *testing.T) {
+	r := require.New(t)
+
+	testStruct := struct {
+		Sub struct {
+			Port int
+		}
+	}{}
+
+	config := Collector{
+		Files: ConfigFiles{
+			BaseName: "config",
+		},
+	}
+
+	// read config from json file
+	tempDir := t.TempDir()
+	r.NoError(ioutil.WriteFile(tempDir+"/config.json", []byte(`{"overwrite": 4000}`), 0600))
+	config.Files.Locations = []string{tempDir}
+
+	r.NoError(config.Get(&testStruct))
+	r.NotEqual(4000, testStruct.Sub.Port)
+
+	// nested
+	r.NoError(ioutil.WriteFile(tempDir+"/config.json", []byte(`{"sub": {overwrite": 4000}}`), 0600))
+	r.NoError(config.Get(&testStruct))
+	r.Equal(4000, testStruct.Sub.Port)
+}
+
 func TestCollector_Get_Env(t *testing.T) {
 	r := require.New(t)
 
@@ -133,6 +162,30 @@ func TestCollector_Get_Env(t *testing.T) {
 	r.NoError(os.Setenv("PORT", "5000"))
 	r.NoError(config.Get(&testStruct))
 	r.Equal(5000, testStruct.Port)
+}
+
+func TestCollector_Get_Env_Nested(t *testing.T) {
+	r := require.New(t)
+
+	testStruct := struct {
+		Sub struct {
+			Port int
+		}
+	}{}
+
+	config := Collector{
+		Flags: true,
+	}
+
+	// short name flag
+	// read env, overwrites files
+	r.NoError(os.Setenv("PORT", "5000"))
+	r.NoError(config.Get(&testStruct))
+	r.NotEqual(5000, testStruct.Sub.Port)
+
+	r.NoError(os.Setenv("SUB_PORT", "5000"))
+	r.NoError(config.Get(&testStruct))
+	r.Equal(5000, testStruct.Sub.Port)
 }
 
 func TestCollector_Get_Env_Overwrite(t *testing.T) {
@@ -174,6 +227,36 @@ func TestCollector_Get_Flags(t *testing.T) {
 
 	r.NoError(config.Get(&testStruct))
 	r.Equal(4567, testStruct.Port)
+}
+
+func TestCollector_Get_Nested(t *testing.T) {
+	r := require.New(t)
+
+	testStruct := struct {
+		Sub struct {
+			Port int
+		}
+	}{}
+
+	config := Collector{
+		Flags: true,
+	}
+
+	// short name flag
+	os.Args = []string{"commandName", "-p", "1234"}
+
+	r.Error(config.Get(&testStruct))
+
+	// full name flag
+	os.Args = []string{"commandName", "--port", "4567"}
+
+	r.Error(config.Get(&testStruct))
+
+	// nested full name
+	os.Args = []string{"commandName", "--sub.port", "8910"}
+
+	r.NoError(config.Get(&testStruct))
+	r.Equal(8910, testStruct.Sub.Port)
 }
 
 func TestCollector_Get_Flags_Overwrite(t *testing.T) {
