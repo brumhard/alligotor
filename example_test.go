15b06b2068fa85fdbeddd176acf0c9df30a03dbe
brumhard <fobytobi@googlemail.com>
2020-12-28 09:49:24 +0100

    Add example for basic usage

---
+++ example_test.go
@@ -0,0 +1,44 @@
+package alligotor_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/brumhard/alligotor"
+	"go.uber.org/zap/zapcore"
+)
+
+type Config struct {
+	SomeList []string
+	SomeMap  map[string]string
+	API      struct {
+		Enabled  bool
+		LogLevel zapcore.Level
+	}
+	DB struct {
+		HostName string
+		Timeout  time.Duration
+	}
+}
+
+func Example() {
+	// Preconditions
+	// Reading from os args and env vars in this example but it would also be possible
+	// to use config file in the current directory (by default).
+	os.Args = []string{"cmdName", "--somelist", "a,b,c", "--api-enabled", "true", "--api-loglevel", "debug"}
+	_ = os.Setenv("SOMEMAP", "a=a,b=b,c=c")
+	_ = os.Setenv("DB_HOSTNAME", "somedb")
+	_ = os.Setenv("DB_TIMEOUT", "1m0s")
+
+	var cfg Config
+	if err := alligotor.Get(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cfg)
+
+	// Output:
+	// {[a b c] map[a:a b:b c:c] {true debug} {somedb 1m0s}}
+}
